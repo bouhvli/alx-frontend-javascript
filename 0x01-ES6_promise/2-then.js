e8227f1229d49a0be99f96a8da0b7eda8762e40b
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-03 13:35:22 +0100

    update the code for task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 /* eslint-disable func-names */
 export default function handleResponseFromAPI(promise) {
   // eslint-disable-next-line no-unused-expressions
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return ({ status: 200, body: 'Success' });
-    // eslint-disable-next-line no-sequences, function-paren-newline
-  }).catch(() => {
-    console.log('Got a response from the API');
-    return (new Error());
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
