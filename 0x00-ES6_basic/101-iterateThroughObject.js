4300fe64f38f93df0c455cc334f3ab5514282b1e
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-03 10:51:42 +0100

    update the code base

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const list = [];
-  for (const item of reportWithIterator) {
-    list.push(item);
-  }
-  return list.join(' | ');
+  return reportWithIterator.join(' | ');
 }
