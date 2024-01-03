988863db61622e48d29ebb66c66e5c5892b0f1ca
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-03 10:47:54 +0100

    update the method

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,14 +1,10 @@
 export default function createReportObject(employeesList) {
   const obj = {
-    allEmployees: employeesList,
-    getNumberOfDepartments() {
-      let count = 0;
-      for (const idx in employeesList) {
-        if (Object.hasOwn(employeesList, idx)) {
-          count += 1;
-        }
-      }
-      return count;
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeeList) {
+      return Object.keys(employeeList).length;
     },
   };
   return obj;
