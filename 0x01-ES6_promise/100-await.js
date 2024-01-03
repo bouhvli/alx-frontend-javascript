558c4c3aa3085fe9eb20c79dd604a3fcdb29bda6
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-03 13:59:45 +0100

    update the object

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,15 +2,15 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const [resPhoto, resUser] = await Promise.all([uploadPhoto(), createUser()]);
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     return {
-      resPhoto,
-      resUser,
+      photo,
+      user,
     };
   } catch (error) {
     return {
-      resPhoto: null,
-      resUser: null,
+      photo: null,
+      user: null,
     };
   }
 }
