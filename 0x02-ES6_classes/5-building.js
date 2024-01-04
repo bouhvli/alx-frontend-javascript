8d078aaa1cd23d7f890a7817e119daaf16a16de9
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-04 14:12:04 +0100

    update 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,15 +2,15 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building
-      && this.evacuationWarningMessage !== 'function') {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
-    }
   }
 
   get sqft() {
     return (this._sqft);
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
   }
 }
